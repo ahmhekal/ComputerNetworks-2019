ade6e098d6032d74341237737eefc0f7187b183f
unknown <doaakamel255@gmail.com>
2018-11-21 16:22:26 +0200

    assignment.py

---
+++ network_assignment.py
@@ -0,0 +1,151 @@
+
+
+def xor(a, b):
+
+    result = []
+
+    for i in range(1, len(b)):
+        if a[i] == b[i]:
+            result.append('0')
+        else:
+            result.append('1')
+
+    return ''.join(result)
+
+
+
+
+def mod2div(divident, divisor):
+
+    pick = len(divisor)
+
+    tmp = divident[0 : pick]
+
+    while pick < len(divident):
+
+        if tmp[0] == '1':
+
+            tmp = xor(divisor, tmp) + divident[pick]
+
+        else:
+            tmp = xor('0'*pick, tmp) + divident[pick]
+
+        pick += 1
+
+
+    if tmp[0] == '1':
+        tmp = xor(divisor, tmp)
+    else:
+        tmp = xor('0'*pick, tmp)
+
+    checkword = tmp
+    return checkword
+
+
+
+
+def Generator(data, key):
+
+    l_key = len(key)
+    appended_data = data + '0'*(l_key-1)
+    remainder = mod2div(appended_data, key)
+
+    codeword = data + remainder
+    print("Remainder : ", remainder)
+    print("Transmitted message (Data + Remainder) : ",
+          codeword)
+    return codeword
+
+
+
+
+def verifier(codeword, key):
+    reminder=mod2div(codeword, key)
+    if int(reminder)==0:
+        print("correct")
+
+    else:
+        print("incorrect")
+
+
+
+
+
+def alter(index,codeword) :
+    bit=int(codeword[index-1])^1
+    codeword=codeword[:index-1]+str(bit)+codeword[index:]
+    return codeword
+
+
+
+
+
+def CRC (command,data,key):
+    if "alter" in command:
+        index=command.find("alter")
+        index=index+6
+        index=int(command[index])
+        codeword=Generator(data, key)
+        codeword=alter(index,codeword)
+        verifier(codeword, key)
+
+    else:
+        codeword=Generator(data, key)
+        verifier(codeword, key)
+
+
+
+"""
+# generator test case
+
+message="1101011111"
+key="10011"
+Generator(message, key)
+
+
+# In[27]:
+
+
+# alter test case
+Transmitted_m="11001"
+index=3
+new=alter(index,Transmitted_m)
+new
+
+
+# In[28]:
+
+
+# verifier without alter test case
+message="1101011111"
+key="10011"
+codeword=Generator(message, key)
+verifier(codeword, key)
+
+
+# In[29]:
+
+
+# verifier with alter test case
+message="1101011111"
+key="10011"
+codeword=Generator(message, key)
+codeword_after_alter=alter(3,codeword)
+print("message after alter",codeword_after_alter)
+verifier(codeword_after_alter, key)
+
+"""
+# # Program
+
+# In[ ]:
+
+
+message=input("please enter a message ")
+key= input("please enter a key ")
+command= input("enter a command")
+
+
+# In[ ]:
+
+
+CRC (command,message,key)
